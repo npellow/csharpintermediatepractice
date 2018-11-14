f070fabe69d6505960d60d0117b9dd0f9b005327
Nichole Pellow <ladypellow@gmail.com>
2018-11-14 11:50:05 -0500

    Section 2
    Lecture 7: Intro to Classes practice

---
+++ Lecture7/Lecture7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture7
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}.", to, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = Person.Parse("Avery");
+            person.Introduce("Nichole");
+        }
+    }
+}
